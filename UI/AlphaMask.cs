b850b0d0063bf77f868f803d0512e11ce50607a3
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-05-23 19:07:41 +0900

    Create AlphaMask script

---
+++ UI/AlphaMask.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+	public class AlphaMask : MonoBehaviour
+	{
+		// 인스펙터 노출 변수
+		// 수치
+		public float 		defaultAlpha = 1f;			// 알파
+		
+		// 인스펙터 비노출 변수
+		// 일반
+		private Image[] 	imageArray;
+		private Text[] 		textArray;
+
+		
+		// 초기화
+		private void Awake()
+		{
+			imageArray = GetComponentsInChildren<Image>();
+			textArray  = GetComponentsInChildren<Text>();
+		}
+		
+		// 시작
+		private void Start()
+		{
+			SetAlpha(defaultAlpha);
+		}
+
+		// 알파 설정
+		public void SetAlpha(float alpha)
+		{
+			Color newColor;
+				
+			
+			foreach (Image target in imageArray)
+			{
+				newColor = target.color;
+				newColor.a = alpha;
+				
+				target.color = newColor;
+			}
+
+			foreach (Text target in textArray)
+			{
+				newColor = target.color;
+				newColor.a = alpha;
+
+				target.color = newColor;
+			}
+		}
+	}
+}
